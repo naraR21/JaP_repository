198465d6cf2f73e5c30bd4904c4a5f28dbadb94a
nara21 <nararamospienlli@gmail.com>
2021-08-20 09:48:21 -0300

    primera entrega
    esta es la primera entrega

---
+++ js/products.js
@@ -0,0 +1,61 @@
+function showProductsList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        /*if (((minCount == undefined) || (minCount != undefined && parseInt(product.productCount) >= minCount)) &&
+        ((maxCount == undefined) || (maxCount != undefined && parseInt(product.productCount) <= maxCount))){*/
+
+
+             htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">` + product.currency + product.cost + `</small>
+                        </div>
+                        <p class="mb-1">` + product.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        //}
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+/*function sortAndShowProducts(sortCriteria, productsArray){
+    currentSortCriteria = sortCriteria;
+
+    if(productsArray != undefined){
+        currentProductsArray = productsArray;
+    }
+
+    currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+    //Muestro las categorías ordenadas
+    showProductsList();
+}*/
+
+
+
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productsArray = resultObj.data;
+            
+            showProductsList(productsArray);
+        }
+    });
+});
